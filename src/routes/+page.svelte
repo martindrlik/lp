093debd36aec35882ee85e87fa079256e89bfa33
Martin Kopka Drlík <m.drlik@quadient.com>
2025-05-29 08:21:19 +0200

    use app/state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { page } from '$app/stores';
-	const isApproved = $page.url.searchParams.has('approved');
+	import { page } from '$app/state';
+	const isApproved = page.url.searchParams.has('approved');
 </script>
 
 <div class="flex h-screen items-center justify-center">
